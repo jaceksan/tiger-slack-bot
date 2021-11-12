7d649cbc86aab6caa43651f8e34d8c98256730dc
Pavel Cerny <pavel.cerny@gooddata.com>
2021-11-12 10:04:00 +0100

    track errors in thread

--- tiger/report_exec.py
+++ tiger/report_exec.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+import traceback
 from tabulate import tabulate
 from tiger.report import Report
 
@@ -8,29 +9,33 @@ TOKEN = os.environ.get('TIGER_API_TOKEN')
 
 
 def process_report_exec(metadata_client, slack_client, re_report, report_match, text, channel_id, workspace_id):
-    # Init Report(Pandas) SDK
-    report_client = Report(ENDPOINT, TOKEN, workspace_id, metadata_client)
-    request = report_client.parse_request(re_report, text)
-    if request:
-        df = report_client.execute(request['metrics'], request['labels'])
-        base_path = '/tmp/' + str(uuid.uuid4())
-        if report_match.group(1) == 'tab':
-            file_path = base_path + '.txt'
-            with open(file_path, 'wt') as fd:
-                fd.write(tabulate(df, headers='keys', tablefmt='psql', showindex="never"))
-        elif report_match.group(1) == 'csv':
-            file_path = base_path + '.csv'
-            with open(file_path, 'wt') as fd:
-                df.to_csv(fd, index=False)
+    try:
+        # Init Report(Pandas) SDK
+        report_client = Report(ENDPOINT, TOKEN, workspace_id, metadata_client)
+        request = report_client.parse_request(re_report, text)
+        if request:
+            df = report_client.execute(request['metrics'], request['labels'])
+            base_path = '/tmp/' + str(uuid.uuid4())
+            if report_match.group(1) == 'tab':
+                file_path = base_path + '.txt'
+                with open(file_path, 'wt') as fd:
+                    fd.write(tabulate(df, headers='keys', tablefmt='psql', showindex="never"))
+            elif report_match.group(1) == 'csv':
+                file_path = base_path + '.csv'
+                with open(file_path, 'wt') as fd:
+                    df.to_csv(fd, index=False)
+            else:
+                file_path = base_path + '.png'
+                print(f"Creating file {file_path} for {text}")
+                with open(file_path, 'w+b') as fd:
+                    plot = report_client.plot_vis(df, request['labels'], request['metrics'])
+                    plot.savefig(fd)
+                print(f"Finished file {file_path} for {text}")
+            slack_client.send_file(channel_id, file_path)
         else:
-            file_path = base_path + '.png'
-            print(f"Creating file {file_path} for {text}")
-            with open(file_path, 'w+b') as fd:
-                plot = report_client.plot_vis(df, request['labels'], request['metrics'])
-                plot.savefig(fd)
-            print(f"Finished file {file_path} for {text}")
-        slack_client.send_file(channel_id, file_path)
-    else:
-        slack_client.send_markdown_message(
-            channel_id, ['ERROR: invalid execute request, valid is {metric} BY {dimension}\n']
-        )
+            slack_client.send_markdown_message(
+                channel_id, ['ERROR: invalid execute request, valid is {metric} BY {dimension}\n']
+            )
+    except Exception:
+        print(traceback.format_exc())
+        slack_client.send_markdown_message(channel_id, [f"Execution of request `{text}` failed.\n"])
