e9517dd1b1e8af8268c178ce9f1f29289e875a5e
Pavel Cerny <pavel.cerny@gooddata.com>
2021-11-12 08:46:30 +0100

    ADD try catch

--- slackbot.py
+++ slackbot.py
@@ -129,98 +129,101 @@ def reply(payload):
     source_user_id = event.get("user", None)
     hit = False
 
-    answered, history_msg = answer_from_history(source_user_id, text)
-    if history_msg:
-        slack_client.send_message(channel_id, history_msg, thread_id)
-    if answered:
-        return
+    try:
+        answered, history_msg = answer_from_history(source_user_id, text)
+        if history_msg:
+            slack_client.send_message(channel_id, history_msg, thread_id)
+        if answered:
+            return
 
-    # Init metadata SDK
-    metadata_client = Metadata(ENDPOINT, TOKEN)
+        # Init metadata SDK
+        metadata_client = Metadata(ENDPOINT, TOKEN)
 
-    channels_2ws = read_config_from_file('slack_channels_2ws.yaml')
-    if channel_id in channels_2ws:
-        workspace_id = channels_2ws[channel_id]
-    else:
-        slack_client.send_markdown_message(
-            channel_id,
-            [f"Error: channel ID {channel_id} is not configured as GoodData.CN workspace\n"]
-        )
-        return
+        channels_2ws = read_config_from_file('slack_channels_2ws.yaml')
+        if channel_id in channels_2ws:
+            workspace_id = channels_2ws[channel_id]
+        else:
+            slack_client.send_markdown_message(
+                channel_id,
+                [f"Error: channel ID {channel_id} is not configured as GoodData.CN workspace\n"]
+            )
+            return
 
-    workspace_ids = metadata_client.get_workspace_ids()
-    if workspace_id not in workspace_ids:
-        slack_client.send_markdown_message(
-            channel_id,
-            [f"Error: workspace {workspace_id} configured for channel {channel_id} does not exist\n"]
-        )
-        return
-    metadata_client.workspace_id = workspace_id
+        workspace_ids = metadata_client.get_workspace_ids()
+        if workspace_id not in workspace_ids:
+            slack_client.send_markdown_message(
+                channel_id,
+                [f"Error: workspace {workspace_id} configured for channel {channel_id} does not exist\n"]
+            )
+            return
+        metadata_client.workspace_id = workspace_id
 
-    help_re = re.compile(r'^<[^>]+> help')
-    if help_re.match(text):
-        if "help exec" in text:
+        help_re = re.compile(r'^<[^>]+> help')
+        if help_re.match(text):
+            if "help exec" in text:
+                hit = True
+                help_hints = [
+                    ":speech_balloon: Thank you for asking, here is detailed doc about `exec_(tab,csv,vis)`:\n\n" +
+                    "* Usage: `exec_*` metric1, metric2 BY label1, label2\n" +
+                    "\t* You can use only 1 metric / 1 label\n" +
+                    "\t* metric/label must be in form of ID with prefix, e.g. label/date.month or fact/order_lines.price\n" +
+                    "* Use `list labels/metrics` to get IDs\n" +
+                    "* Types:\n" +
+                    "\t* `tab` - pretty printed table\n" +
+                    "\t* `csv` - CSV file as attachment\n" +
+                    "\t* `vis` - chart rendered by `matplotlib`\n" +
+                    ":raised_hands:\n"
+                ]
+                slack_client.send_markdown_message(channel_id, help_hints, thread_id)
+            elif "help" in text:
+                hit = True
+                help_hints = [
+                    ":speech_balloon: Thank you for asking, there are few hints I can help you with:\n\n" +
+                    "* `list workspaces` \n" +
+                    "* `list data sources`\n" +
+                    "* `list labels`\n" +
+                    "* `list metrics`\n" +
+                    "* `list insights`\n" +
+                    "* `execute_tab`/`execute_csv`/`execute_vis|sea` - returns table, csv or vizualization\n\n" +
+                    " use `help exec` to learn more about it"
+                    ":raised_hands:\n"
+                ]
+                slack_client.send_markdown_message(channel_id, help_hints, thread_id)
+
+        as_file_flag = "as file" in text
+        if "list workspaces" in text:
             hit = True
-            help_hints = [
-                ":speech_balloon: Thank you for asking, here is detailed doc about `exec_(tab,csv,vis)`:\n\n" +
-                "* Usage: `exec_*` metric1, metric2 BY label1, label2\n" +
-                "\t* You can use only 1 metric / 1 label\n" +
-                "\t* metric/label must be in form of ID with prefix, e.g. label/date.month or fact/order_lines.price\n" +
-                "* Use `list labels/metrics` to get IDs\n" +
-                "* Types:\n" +
-                "\t* `tab` - pretty printed table\n" +
-                "\t* `csv` - CSV file as attachment\n" +
-                "\t* `vis` - chart rendered by `matplotlib`\n" +
-                ":raised_hands:\n"
-            ]
-            slack_client.send_markdown_message(channel_id, help_hints, thread_id)
-        elif "help" in text:
+            workspaces = metadata_client.list_workspaces()
+            send_tabulated_result(channel_id, 'Workspaces:\n-------\n', workspaces, thread_id, as_file_flag)
+        if "list data sources" in text:
             hit = True
-            help_hints = [
-                ":speech_balloon: Thank you for asking, there are few hints I can help you with:\n\n" +
-                "* `list workspaces` \n" +
-                "* `list data sources`\n" +
-                "* `list labels`\n" +
-                "* `list metrics`\n" +
-                "* `list insights`\n" +
-                "* `execute_tab`/`execute_csv`/`execute_vis|sea` - returns table, csv or vizualization\n\n" +
-                " use `help exec` to learn more about it"
-                ":raised_hands:\n"
-            ]
-            slack_client.send_markdown_message(channel_id, help_hints, thread_id)
+            data_sources = metadata_client.list_data_sources()
+            send_tabulated_result(channel_id, 'Data sources:\n-------\n', data_sources, thread_id, as_file_flag)
+        if "list labels" in text:
+            hit = True
+            labels = metadata_client.list_labels()
+            send_tabulated_result(channel_id, 'Labels:\n-------\n', labels, thread_id, as_file_flag)
+        if "list metrics" in text:
+            hit = True
+            metrics = metadata_client.list_metrics()
+            facts = metadata_client.list_facts()
+            send_tabulated_result(channel_id, 'Metrics:\n-------\n', metrics, thread_id, as_file_flag)
+            send_tabulated_result(channel_id, 'Facts:\n-------\n', facts, thread_id, as_file_flag)
+        if "list insights" in text:
+            hit = True
+            insights = metadata_client.list_insights()
+            send_tabulated_result(channel_id, 'Insights:\n-------\n', insights, thread_id, as_file_flag)
 
-    as_file_flag = "as file" in text
-    if "list workspaces" in text:
-        hit = True
-        workspaces = metadata_client.list_workspaces()
-        send_tabulated_result(channel_id, 'Workspaces:\n-------\n', workspaces, thread_id, as_file_flag)
-    if "list data sources" in text:
-        hit = True
-        data_sources = metadata_client.list_data_sources()
-        send_tabulated_result(channel_id, 'Data sources:\n-------\n', data_sources, thread_id, as_file_flag)
-    if "list labels" in text:
-        hit = True
-        labels = metadata_client.list_labels()
-        send_tabulated_result(channel_id, 'Labels:\n-------\n', labels, thread_id, as_file_flag)
-    if "list metrics" in text:
-        hit = True
-        metrics = metadata_client.list_metrics()
-        facts = metadata_client.list_facts()
-        send_tabulated_result(channel_id, 'Metrics:\n-------\n', metrics, thread_id, as_file_flag)
-        send_tabulated_result(channel_id, 'Facts:\n-------\n', facts, thread_id, as_file_flag)
-    if "list insights" in text:
-        hit = True
-        insights = metadata_client.list_insights()
-        send_tabulated_result(channel_id, 'Insights:\n-------\n', insights, thread_id, as_file_flag)
+        re_report = re.compile(r'^<[^>]+>\s*execute_(tab|csv|vis) ', re.I)
+        report_match = re_report.match(text)
+        if report_match:
+            hit = True
+            process_report_exec(metadata_client, re_report, report_match, text, channel_id, workspace_id)
 
-    re_report = re.compile(r'^<[^>]+>\s*execute_(tab|csv|vis) ', re.I)
-    report_match = re_report.match(text)
-    if report_match:
-        hit = True
-        process_report_exec(metadata_client, re_report, report_match, text, channel_id, workspace_id)
-
-    if not hit:
-        slack_client.send_markdown_message(channel_id, [f"Hello, thanks for mentioning me <@{source_user_id}>.\n"])
+        if not hit:
+            slack_client.send_markdown_message(channel_id, [f"Hello, thanks for mentioning me <@{source_user_id}>.\n"])
+    except Exception:
+        slack_client.send_markdown_message(channel_id, [f"Something went wrong user <@{source_user_id}>, fix me\n"])
 
 
 @app.route("/")
