fbb5f71453ceacbcdd38a8036bbfbd49e56d3f31
Jacek <jacek@gooddata.com>
2021-11-12 08:05:16 +0100

    fix threshold

--- scheduler.py
+++ scheduler.py
@@ -15,7 +15,7 @@ ENDPOINT = 'https://hackaton.anywhere.gooddata.com'
 TOKEN = os.environ.get('TIGER_API_TOKEN')
 WORKSPACE_ID = 'demo'
 TARGET_CHANNEL_ID = 'C02M4PF2R8C'
-THRESHOLD = 260000
+THRESHOLD = 230000
 DATA_SOURCE_ID = 'pg_staging-demo'
 
 # Initialize a Web API client
